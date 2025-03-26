c6a93dc0db9ca9c673017e43f10d2178dc5c1e17
kkeef11 <kkeefer@cenergistic.com>
2025-03-26 12:44:21 -0500

    fixed apostraphe build error

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -22,7 +22,7 @@ const Page = () => {
           paddingBottom="3rem"
           textAlign="center"
         >
-          Hey, my name's Kevin
+          Hey, my name&apos;s Kevin
         </Typography>
       </Grid2>
       <Grid2
@@ -30,11 +30,11 @@ const Page = () => {
         sx={{ paddingLeft: "1rem", paddingRight: "1rem" }}
       >
         <Typography variant="body1" color="white" fontFamily="Open Sans">
-          I'm a software engineer with six years of experience specializing in
-          full-stack development, primarily focused on React.js and TypeScript
-          for the frontend and Node.js for the backend. I have hands-on
-          experience working with both the MERN (MongoDB, Express, React,
-          Node.js) and PERN (PostgreSQL, Express, React, Node.js) stacks.
+          I&apos;m a software engineer with six years of experience specializing
+          in full-stack development, primarily focused on React.js and
+          TypeScript for the frontend and Node.js for the backend. I have
+          hands-on experience working with both the MERN (MongoDB, Express,
+          React, Node.js) and PERN (PostgreSQL, Express, React, Node.js) stacks.
         </Typography>
         <Typography
           variant="body1"
@@ -45,8 +45,8 @@ const Page = () => {
           My current tech stack includes TypeScript, React.js, Node.js,
           PostgreSQL (RDBMS), Prisma ORM, and Elasticsearch (NoSQL). I also have
           experience working with AWS cloud infrastructure and managing CI/CD
-          via GitHub actions. I'm passionate about building scalable, efficient
-          applications and as well as bringing UI/UX designs to life.
+          via GitHub actions. I&apos;m passionate about building scalable,
+          efficient applications and as well as bringing UI/UX designs to life.
         </Typography>
         <Box display="flex" justifyContent="center" paddingTop="1.5rem">
           <Typography variant="body1" color="white" fontFamily="Open Sans">
